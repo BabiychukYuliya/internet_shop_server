50ac8c1247506edfe77ca93d11042fdd8a975bec
BabiychukYuliya <julia4233097@gmail.com>
2024-01-04 23:56:43 +0200

    register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,26 @@
 const HttpError = require("../helpers/httpError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Bascet } = require("../models/models");
 
-const register = async (req, res) => {};
+const register = async (req, res, next) => {
+  const { email, password, role } = req.body;
+
+  if (!email || !password) {
+    return next(HttpError(400, "Invalid email or password"));
+  }
+  const candidate = await User.findOne({ where: { email } });
+
+  if (candidate) {
+    return next(HttpError(400, "User already registered"));
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ email, role, password: hashPassword });
+  const basket = await Bascet.create({ userId: user.id });
+  const jwt = jwt.sign({ id: user.id, email, role });
+};
 
 const login = async (req, res) => {};
 
